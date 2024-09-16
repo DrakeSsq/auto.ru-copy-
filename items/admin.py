bbbbf3658c9400c4e12fa458a9377959af36f98b
DrakeSsq <daniil.ershovvv73@mail.ru>
2024-09-16 23:05:20 +0300

    add legkovoe_avto model

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Legkovoe_Avto, Marka_Legkovoe_Avto, Model_Legkovoe_Avto, Transport
 # Register your models here.
+
+
+
+@admin.register(Legkovoe_Avto)
+class Legkovoe_Avto_Admin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Marka_Legkovoe_Avto)
+admin.site.register(Model_Legkovoe_Avto)
+admin.site.register(Transport)
--- items/models.py
+++ items/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from django.utils.crypto import get_random_string
+from transliterate import slugify
 
-# Create your models here.
+
+class Transport(MPTTModel):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    parent = TreeForeignKey('self', on_delete=models.PROTECT, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            while Transport.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{get_random_string(8)}"
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class Marka_Legkovoe_Avto(MPTTModel):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    parent = TreeForeignKey(Transport, on_delete=models.PROTECT)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            while Marka_Legkovoe_Avto.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{get_random_string(8)}"
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class Model_Legkovoe_Avto(MPTTModel):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    parent = TreeForeignKey(Marka_Legkovoe_Avto, on_delete=models.PROTECT)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            while Model_Legkovoe_Avto.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{get_random_string(8)}"
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class Legkovoe_Avto(MPTTModel):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    deskription = models.TextField()
+    mileage = models.IntegerField()
+    year = models.IntegerField()
+    price = models.IntegerField()
+    type = TreeForeignKey(Transport, on_delete=models.PROTECT)
+    marka = TreeForeignKey(Marka_Legkovoe_Avto, on_delete=models.PROTECT)
+    parent = TreeForeignKey(Model_Legkovoe_Avto, on_delete=models.PROTECT)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            while Legkovoe_Avto.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{get_random_string(8)}"
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+class Image_Legkovoe_Avto(models.Model): 
+    avto = models.ForeignKey(Legkovoe_Avto, on_delete=models.CASCADE)
+    images = models.FileField()
